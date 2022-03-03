66c74c855b614d0806a839601a5a323067211664
atnomoverflow <montaser-abdmajid.zehri@enis.tn>
2022-03-03 09:06:27 +0100

    ai part done 100%

---
+++ lib/ui/home_view.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:obj_detect/tflite/recognition.dart';
+import 'package:obj_detect/tflite/stats.dart';
+import 'package:obj_detect/ui/box_widget.dart';
+import 'package:obj_detect/ui/camera_view_singleton.dart';
+
+import 'camera_view.dart';
+
+/// [HomeView] stacks [CameraView] and [BoxWidget]s with bottom sheet for stats
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  /// Results to draw bounding boxes
+  List<Recognition>? results;
+
+  /// Realtime stats
+  Stats? stats;
+
+  /// Scaffold Key
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          // Camera View
+          CameraView(resultsCallback, statsCallback),
+
+          // Bounding boxes
+          boundingBoxes(results),
+
+          // Heading
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                'Object Detection Flutter',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepOrangeAccent.withOpacity(0.6),
+                ),
+              ),
+            ),
+          ),
+
+        
+        ],
+      ),
+    );
+  }
+
+  /// Returns Stack of bounding boxes
+  Widget boundingBoxes(List<Recognition>? results) {
+    if (results == null) {
+      return Container();
+    }
+    return Stack(
+      children: results
+          .map((e) => BoxWidget(
+                result: e,
+              ))
+          .toList(),
+    );
+  }
+
+  /// Callback to get inference results from [CameraView]
+  void resultsCallback(List<Recognition> results) {
+    setState(() {
+      this.results = results;
+    });
+  }
+
+  /// Callback to get inference stats from [CameraView]
+  void statsCallback(Stats stats) {
+    setState(() {
+      this.stats = stats;
+    });
+  }
+
+ }
+
+/// Row for one Stats field
