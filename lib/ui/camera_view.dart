6c4ff04ce546d7db96b1ebac6e68522de40d1c7f
atnomoverflow <montaser-abdmajid.zehri@enis.tn>
2022-03-15 08:28:58 +0100

    merging the ar core with tflite

--- lib/ui/camera_view.dart
+++ lib/ui/camera_view.dart
@@ -56,6 +56,63 @@ class _CameraViewState extends State<CameraView> with WidgetsBindingObserver {
           try {
             String imagePath = await arCoreController?.snapshot() ?? "";
 
+            /// Callback to receive each frame [CameraImage] perform inference on it
+            onLatestImageAvailable(CameraImage cameraImage) async {
+              if (classifier?.interpreter != null &&
+                  classifier?.labels != null) {
+                // If previous inference has not completed then return
+                if (predicting!) {
+                  return;
+                }
+
+                setState(() {
+                  predicting = true;
+                });
+
+                var uiThreadTimeStart = DateTime.now().millisecondsSinceEpoch;
+                List<String> labels;
+
+                labels = classifier?.labels ?? [];
+
+                // Data to be passed to inference isolate
+                var isolateData = IsolateData(
+                    cameraImage, classifier?.interpreter?.address ?? 0, labels);
+
+                // We could have simply used the compute method as well however
+                // it would be as in-efficient as we need to continuously passing data
+                // to another isolate.
+
+                /// perform inference in separate isolate
+                Map<String, dynamic> inferenceResults =
+                    await inference(isolateData);
+
+                var uiThreadInferenceElapsedTime =
+                    DateTime.now().millisecondsSinceEpoch - uiThreadTimeStart;
+
+                // pass results to HomeView
+                widget.resultsCallback(inferenceResults["recognitions"]);
+
+                // pass stats to HomeView
+                widget.statsCallback((inferenceResults["stats"] as Stats)
+                  ..totalElapsedTime = uiThreadInferenceElapsedTime);
+
+                // set predicting to false to allow new frames
+                setState(() {
+                  predicting = false;
+                });
+              }
+            }
+
+            /// Runs inference in another isolate
+            Future<Map<String, dynamic>> inference(
+                IsolateData isolateData) async {
+              ReceivePort responsePort = ReceivePort();
+              isolateUtils?.sendPort
+                  .send(isolateData..responsePort = responsePort.sendPort);
+              var results = await responsePort.first;
+              return results;
+            }
+
             final String fileName = "${Random().nextInt(10000)}.jpg";
             final File file = File(imagePath);
             var ref = firebase_storage.FirebaseStorage.instance
